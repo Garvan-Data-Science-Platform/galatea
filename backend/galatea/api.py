cde3282f2d3e1e6faad9b716ec11c2b780f6f752
TIM KALLADY <tim@piccard.ai>
2023-08-15 18:03:57 +1000

    initial commit

---
+++ backend/galatea/api.py
@@ -0,0 +1,40 @@
+from ninja import NinjaAPI
+from ninja.errors import AuthenticationError
+from google.cloud import storage
+from .auth import AuthBearer
+from .schemas import BucketFileList
+from typing import List
+import datetime
+
+api = NinjaAPI()
+
+storage_client = storage.Client.from_service_account_json(
+    "/etc/secret-volume/cabana-dev-388204-ae3495fa8567.json")
+
+
+@api.get("/bucket/{bucket}/", response=BucketFileList, auth=AuthBearer())
+def list_bucket(request, bucket: str, subdir: str | None = None, limit: int | None = None):
+    if subdir:
+        prefix = subdir + "/"
+    else:
+        prefix = None
+
+    if limit is None:
+        limit = -1
+
+    results = storage_client.list_blobs(bucket, delimiter="/", prefix=prefix)
+    blobs = [r for i, r in enumerate(results) if i < limit and r.name != str(subdir) + '/']
+    files = [{'name': b.name, 'url': b.generate_signed_url(version='v4', expiration=datetime.timedelta(minutes=15), method='GET')} for b in blobs]
+    folders: List[str] = list(results.prefixes)
+
+    return {"files": files, "folders": folders}
+
+
+@api.exception_handler(AuthenticationError)
+def service_unavailable(request, exc: AuthenticationError):
+    exc.args
+    return api.create_response(
+        request,
+        exc.args[1],
+        status=exc.args[0],
+    )
---
+++ backend/galatea/auth.py
@@ -0,0 +1,83 @@
+from types import SimpleNamespace
+from functools import wraps
+from http import HTTPStatus
+from typing import NamedTuple
+import jwt
+import os
+from dotenv import load_dotenv
+from django.http import HttpResponse, JsonResponse
+from ninja.security import HttpBearer
+from ninja.errors import AuthenticationError
+
+load_dotenv()
+
+AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
+AUTH0_AUDIENCE = os.getenv("AUTH0_AUDIENCE")
+
+
+def json_abort(status, message):
+    raise AuthenticationError(status, message)
+
+
+class Auth0Service:
+    """Perform JSON Web Token (JWT) validation using PyJWT"""
+
+    def __init__(self):
+        self.issuer_url = f'https://{AUTH0_DOMAIN}/'
+        self.audience = None
+        self.algorithm = 'RS256'
+        self.jwks_uri = f'{self.issuer_url}.well-known/jwks.json'
+        self.audience = AUTH0_AUDIENCE
+
+    def get_signing_key(self, token):
+        try:
+            jwks_client = jwt.PyJWKClient(self.jwks_uri)
+
+            return jwks_client.get_signing_key_from_jwt(token).key
+        except Exception as error:
+            json_abort(HTTPStatus.INTERNAL_SERVER_ERROR, {
+                "error": "signing_key_unavailable",
+                "error_description": error.__str__(),
+                "message": "Unable to verify credentials"
+            })
+
+    def validate_jwt(self, token):
+        try:
+            jwt_signing_key = self.get_signing_key(token)
+
+            payload = jwt.decode(
+                token,
+                jwt_signing_key,
+                algorithms=self.algorithm,
+                audience=self.audience,
+                issuer=self.issuer_url,
+            )
+        except Exception as error:
+            json_abort(HTTPStatus.UNAUTHORIZED, {
+                "error": "invalid_token",
+                "error_description": error.__str__(),
+                "message": "Bad credentials."
+            })
+            return
+
+        return payload
+
+
+auth0_service = Auth0Service()
+
+
+class AuthBearer(HttpBearer):
+    def authenticate(self, request, token):
+        validated_token = auth0_service.validate_jwt(token)
+        return validated_token
+
+
+unauthorized_error = {
+    "message": "Requires authentication"
+}
+
+invalid_request_error = {
+    "error": "invalid_request",
+    "error_description": "Authorization header value must follow this format: Bearer access-token",
+    "message": "Requires authentication"
+}
