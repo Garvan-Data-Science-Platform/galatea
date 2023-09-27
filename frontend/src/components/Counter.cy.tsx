6f3f60d96d1736ef6ab7186add71853dbc2e027b
TIM KALLADY <tim@piccard.ai>
2023-09-27 14:46:33 +1000

    update

--- frontend/src/components/Counter.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Counter } from "./Counter";
-
-describe("<Counter />", () => {
-  it("renders", () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Counter />);
-  });
-});
